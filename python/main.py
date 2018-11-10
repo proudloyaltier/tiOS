3975badeae60a4d2b2593749d18d8c7872031bf2
proudloyaltier <timediamail@gmail.com>
2018-11-09 16:08:19 -0800

    v0.0.2 Alpha

--- python/main.py
+++ python/main.py
@@ -1,13 +1,37 @@
+# TiOS v0.0.2 Alpha
+
+import os
+import time
+
+from datetime import datetime
 from tkinter import *
 
-class TiOS:
-	def __init__(self, master):
-		self.master = master
-		master.title = "TiOS"
+version = "0.0.2 Alpha"
+
+print("Welcome to TiOS!\nVersion: " + version)
+
+time.sleep(1)
+
+print("\nStarting GUI...")
 
 root = Tk()
 
-root.configure(background = "white")
+root.configure(background = "white", cursor = "none")
 root.attributes("-fullscreen", True)
 
-gui = TiOS(root)
+root.title = "TiOS"
+
+# Wallpaper
+
+background_image = PhotoImage(file = "wallpaper.png")
+background_label = Label(root, image = background_image)
+
+background_label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+background_label.image = background_image
+
+# Time Label
+
+current_time = datetime.now().time()
+
+time_label = Label(root, text = current_time)
+time_label.pack()
