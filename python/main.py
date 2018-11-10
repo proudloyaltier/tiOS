db3534f523cdc2fb9149b0bb81d8f413fb972432
proudloyaltier <timediamail@gmail.com>
2018-11-09 17:01:02 -0800

    v0.0.3 Alpha

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-# TiOS v0.0.2 Alpha
+# TiOS v0.0.3 Alpha
 
 import os
 import time
@@ -6,7 +6,33 @@ import time
 from datetime import datetime
 from tkinter import *
 
-version = "0.0.2 Alpha"
+version = "0.0.3 Alpha"
+
+# Variables
+
+tiles = [{
+	"title": "Test Tile",
+	"text": "This is a random tile!"
+}, {
+	"title": "Another Tile",
+	"text": "Why not have two?"
+}]
+
+# Tile Functions
+
+def openTile(tile):
+	frame = Frame(root, width = 1920, height = 1080, bg = "white")
+	label = Label(text = tile["title"])
+  
+	frame.grid()
+	label.grid(padx = (10, 10), pady = (10, 10))
+
+def updateTiles():
+	for tile in tiles:
+		button = Button(root, text = tile["title"] + "\n\n" + tile["text"], width = 50, height = 20, command = lambda: openTile(tile))
+		button.grid(padx = (10, 10), pady = (10, 10))
+
+# Main
 
 print("Welcome to TiOS!\nVersion: " + version)
 
@@ -16,14 +42,14 @@ print("\nStarting GUI...")
 
 root = Tk()
 
-root.configure(background = "white", cursor = "none")
+#root.configure(background = "white", cursor = "none")
 root.attributes("-fullscreen", True)
 
 root.title = "TiOS"
 
 # Wallpaper
 
-background_image = PhotoImage(file = "../images/wallpaper.png")
+background_image = PhotoImage(file = "wallpaper.png")
 background_label = Label(root, image = background_image)
 
 background_label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
@@ -31,7 +57,9 @@ background_label.image = background_image
 
 # Time Label
 
-current_time = datetime.now().time()
+#current_time = datetime.now().time()
 
-time_label = Label(root, text = current_time)
-time_label.pack()
+#time_label = Label(root, text = current_time)
+#time_label.grid()
+
+updateTiles()
