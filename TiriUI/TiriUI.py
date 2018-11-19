6edc926a3cb4a302996b0b5e6361a6193de6656e
proudloyaltier <timediamail@gmail.com>
2018-11-19 17:48:40 -0500

    Don't need this

--- TiriUI/TiriUI.py
+++ TiriUI/TiriUI.py
@@ -5,7 +5,6 @@ import os
 import random
 import time
 import aiy.audio
-import main
 
 CLIENT_ID = "bPmtPn-lV_rFXuxrww2GOw=="
 CLIENT_KEY = "Vl56r8ewBSDhEdTfpFIWwuxhOqA01lF3aopxGH6aKOAEx9u8DMDpzsPFFed3M5-QaaiDGiMIlo-0p6Zk_1YfTQ=="
@@ -74,4 +73,3 @@ while True:
   if client.fill(samples): break
   
 client.finish()
-main.updateCard()
