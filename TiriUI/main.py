4c01ab21ad27d2532571dadcb8fc0b90b49e98db
proudloyaltier <timediamail@gmail.com>
2018-11-20 09:05:25 -0500

    Add card expand

--- TiriUI/main.py
+++ TiriUI/main.py
@@ -17,11 +17,31 @@ root.attributes("-fullscreen", True)
 root.title = "TiOS"
 
 # Tiri Card
-
+button = PhotoImage(file = "Button.png")
 tiriResponse = PhotoImage(file = "TiriResponse.png")
-tiriCard = Label(root, image=tiriResponse, font = "Roboto 24", anchor="center", text="Hi! I'm Tiri!", compound=CENTER)
+tiriCard = Button(root, bg = "white", image=tiriResponse, font = "Roboto 24", anchor="center", text="Hi! I'm Tiri!", compound=CENTER, command = lambda: openCard())
 tiriCard.pack(pady=(30,30))
-
+    
+def openCard():
+	response = open("TiPodFinalResponse.txt", "r")
+	readResponseOpen = response.read()
+	response.close()
+	expanded_frame = Frame(root, bg = "white")
+	expanded_frame.pack()
+	tile_title = Label(expanded_frame, text = "Tiri Response", font = "Roboto 24", bg = "white")
+	tile_text = Label(expanded_frame, text = readResponseOpen, font = "Roboto 14", bg = "white")
+	tile_button = Button(expanded_frame, text = "Back", font = "Roboto 24", bg = "white", image=button, anchor="center", compound=CENTER, command = lambda: closeCard(expanded_frame, tile_title, tile_text, tile_button))
+	expanded_frame.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+	tile_title.grid(padx = (25, 25), pady = (25, 25))
+	tile_text.grid(padx = (25, 25))
+	tile_button.grid()
+	
+def closeCard(frame, title, text, button):
+	frame.destroy()
+	title.destroy()
+	text.destroy()
+	button.destroy()
+	
 def updateCard():
 	if os.path.isfile('TiPodFinalResponse.txt'):
 		response = open("TiPodFinalResponse.txt", "r")
