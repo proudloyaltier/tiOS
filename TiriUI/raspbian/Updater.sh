423663be3af76ef4146e1128f0b89f4d5a39c3ee
proudloyaltier <timediamail@gmail.com>
2018-12-26 10:05:59 -0500

    Updater test
    Might not work, going to test on hardware

--- TiriUI/raspbian/Updater.sh
+++ TiriUI/raspbian/Updater.sh
@@ -1,5 +1,9 @@
-svn checkout https://github.com/TiriAI/TiOS/trunk/TiriUI
-mv -v TiriUI/* TiOS
-rm -r TiriUI
-cd TiOS
-rm -r raspbian
+{
+svn checkout https://github.com/TiriAI/TiOS/trunk/TiriUI &&
+mv -v TiriUI/* TiOS &&
+rm -r TiriUI &&
+cd TiOS &&
+rm -r raspbian &&
+} || {
+  echo "WiFi Not Connected, updates not available"
+}
