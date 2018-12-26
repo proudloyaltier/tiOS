4264563da14ece328e0d2323b824103593a1426b
proudloyaltier <timediamail@gmail.com>
2018-12-26 13:28:48 -0500

    This works

--- TiriUI/raspbian/updater.sh
+++ TiriUI/raspbian/updater.sh
@@ -2,6 +2,5 @@ svn checkout https://github.com/TiriAI/TiOS/trunk/TiriUI
 mv -v TiriUI/* TiOS
 rm -r TiriUI
 cd TiOS
-sudo chmod u+x TiriUI.py
-sudo chown pi TiriUI.py
+chmod u+x TiriUI.py
 rm -r raspbian
