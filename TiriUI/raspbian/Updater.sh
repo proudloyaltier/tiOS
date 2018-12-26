bbf72589c45cc16be1d96e3d709ff7dc6a9c446f
proudloyaltier <timediamail@gmail.com>
2018-12-26 10:42:35 -0500

    Final updater revision

--- TiriUI/raspbian/Updater.sh
+++ TiriUI/raspbian/Updater.sh
@@ -1,9 +1,6 @@
-{
-svn checkout https://github.com/TiriAI/TiOS/trunk/TiriUI &&
-mv -v TiriUI/* TiOS &&
-rm -r TiriUI &&
-cd TiOS &&
-rm -r raspbian &&
-} || {
-  echo "WiFi Not Connected, updates not available"
-}
+svn checkout https://github.com/TiriAI/TiOS/trunk/TiriUI
+mv -v TiriUI/* TiOS
+rm -r TiriUI
+cd TiOS
+chmod u+x TiriUI.py
+rm -r raspbian
