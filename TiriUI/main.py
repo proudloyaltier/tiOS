f33f88adbaecde1923a2d4cd6a7890902c9055af
proudloyaltier <timediamail@gmail.com>
2019-01-11 16:41:06 -0500

    Multi cards

--- TiriUI/main.py
+++ TiriUI/main.py
@@ -8,12 +8,12 @@ from tkinter import *
 
 # Tkinter Window Configuration
 
+tiles = []
 root = Tk()
 root.configure(background = "white", cursor = "none")
 root.attributes("-fullscreen", True)
 root.title("TiOS")
 
-# Tiri Card
 button = PhotoImage(file = "Button.png")
 tiriMic = PhotoImage(file = "microphone.png")
 tiriMicSpeaking = PhotoImage(file = "microphoneActivated.png")
@@ -28,27 +28,28 @@ startTiriButton.pack(anchor="w", side=TOP)
 realtimeSpeaking = Label(root, text = "", font = "Roboto 24", bg = "white")
 realtimeSpeaking.pack(anchor="center", side=TOP)
 cardFrame = Frame(root)
-tiriCardPowerHelp = Label(cardFrame, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=tiriResponsePowerHelp, font = "Roboto 24", anchor="center", compound=CENTER)
-tiriCardPowerHelp.pack(side=LEFT)
-tiriCardHelp = Label(cardFrame, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=tiriResponseHelp, font = "Roboto 24", anchor="center", compound=CENTER)
-tiriCardHelp.pack(side=LEFT)
-tiriCard = Button(cardFrame, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=tiriResponse, font = "Roboto 24", anchor="center", text="Hi! I'm Tiri!", compound=CENTER, command = lambda: openCard())
-tiriCard.pack(side=LEFT)
-tiriCardWifiHelp = Label(cardFrame, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=tiriResponseWifiHelp, font = "Roboto 24", anchor="center", compound=CENTER)
-tiriCardWifiHelp.pack(side=LEFT)
-cardFrame.pack()
-
-
-def openCard():
-	response = open("TiPodFinalResponse.txt", "r")
-	readResponseOpen = response.read()
-	if (len(readResponseOpen) > 50):
-		readResponseOpen = '\n'.join(readResponseOpen[i:i+50] for i in range(0, len(readResponseOpen), 50))
-	response.close()
+# Tiri Card
+def loadTiles():
+	for widget in cardFrame.winfo_children():
+	    widget.destroy()
+	if len(tiles) >= 5:
+		lengthOfFor = 5
+	else:
+		lengthOfFor = len(tiles)
+	for i in range(0, lengthOfFor):
+		tiriCard = Button(cardFrame, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=tiriResponse, font = "Roboto 24", anchor="center", text="Hi! I'm Tiri!", compound=CENTER, command = lambda x=i: openCard(x))
+		tiriCard.pack(side=LEFT)
+		if (len(tiles[i]) > 5):
+			tiriCard['text'] = tiles[i][0:5] + "..."
+		else:
+			tiriCard['text'] = tiles[i]
+	cardFrame.pack()
+	
+def openCard(i):
 	expanded_frame = Frame(root, bg = "white")
 	expanded_frame.pack()
 	tile_title = Label(expanded_frame, text = "Tiri Response", font = "Roboto 24", bg = "white")
-	tile_text = Label(expanded_frame, text = readResponseOpen, font = "Roboto 14", bg = "white")
+	tile_text = Label(expanded_frame, text = tiles[i], font = "Roboto 14", bg = "white")
 	tile_button = Button(expanded_frame, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, text = "Back", font = "Roboto 24", bg = "white", image=button, anchor="center", compound=CENTER, command = lambda: closeCard(expanded_frame, tile_title, tile_text, tile_button))
 	expanded_frame.place(x = 0, y = 0, relwidth = 1, relheight = 1)
 	tile_title.grid(padx = (25, 25), pady = (25, 25))
@@ -61,32 +62,17 @@ def closeCard(frame, title, text, button):
 	text.destroy()
 	button.destroy()
 	
-def updateCard():
-	if os.path.isfile('TiPodFinalResponse.txt'):
-		response = open("TiPodFinalResponse.txt", "r")
-		readResponse = response.read()
-		response.close()
-		if (len(readResponse) > 5):
-			tiriCard['text'] = readResponse[0:5] + "..."
-		else:
-			tiriCard['text'] = readResponse
-	else:
-		tiriCard['text'] = "Hello! I'm Tiri!"
-	tiriCard.after(1, updateCard)
-
-updateCard()
 	
 # Time Label
 
 clock = Label(root, text = "?:??", font = "Roboto 54", bg = "white")
 
-clock.pack(pady = (5, 5))
 powerOffButton = Button(root, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=power, compound=CENTER, command = lambda: powerOff())
 powerOffButton.pack(side=LEFT)
+clock.pack(pady = (5, 5))
 
 wifiButton = Button(root, relief="flat", highlightthickness=0, activebackground="white", borderwidth=0, bg = "white", image=wifi, compound=CENTER, command = lambda: resetWifi())
 wifiButton.pack(side=RIGHT)
-
 
 def tick():
 	clock.configure(text = time.strftime("%H:%M:%S"))
@@ -132,6 +118,21 @@ def updateSpeaking():
 	
 updateSpeaking()
 
+def updateCard():
+	if os.path.isfile('TiPodFinalResponse.txt'):
+		response = open("TiPodFinalResponse.txt", "r")
+		readResponse = response.read()
+		response.close()
+		if len(readResponse) > 1:
+			tiles.append(readResponse)
+			response = open("TiPodFinalResponse.txt", "w")
+			response.write("")
+			response.close()
+			cardFrame.pack_forget()
+			loadTiles()
+	realtimeSpeaking.after(1, updateCard)
+
+updateCard()
 
 if __name__ == '__main__':
 	root.mainloop()
