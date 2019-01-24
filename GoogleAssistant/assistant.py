c5f4d83fb2aa8ed7850cea7f38b601fd16e16da7
proudloyaltier <timediamail@gmail.com>
2019-01-24 15:03:02 -0500

    Early test

--- GoogleAssistant/assistant.py
+++ GoogleAssistant/assistant.py
@@ -3,13 +3,28 @@
 import logging
 import platform
 import sys
+import subprocess
 
 from google.assistant.library.event import EventType
 
 from aiy.assistant import auth_helpers
 from aiy.assistant.library import Assistant
 from aiy.board import Board, Led
+from aiy.voice import tts
 
+def power_off_pi():
+    tts.say('Good bye!')
+    subprocess.call('sudo shutdown now', shell=True)
+
+
+def reboot_pi():
+    tts.say('See you in a bit!')
+    subprocess.call('sudo reboot', shell=True)
+    
+def reset_wifi():
+    tts.say("Disconnecting now...")
+    subprocess.Popen("sudo python3 /usr/lib/raspiwifi/reset_device/manual_reset.py", shell = True)
+    
 def process_event(led, event):
     logging.info(event)
 
@@ -31,7 +46,21 @@ def process_event(led, event):
           or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
           or event.type == EventType.ON_NO_RESPONSE):
         led.state = Led.BEACON_DARK
-
+    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+        print('You said:', event.args['text'])
+        text = event.args['text'].lower()
+        if text == 'power off':
+            assistant.stop_conversation()
+            power_off_pi()
+        elif text == 'reboot':
+            assistant.stop_conversation()
+            reboot_pi()
+        elif text == 'reset wifi':
+            assistant.stop_conversation()
+            reset_wifi()
+        elif text == 'reset':
+            assistant.stop_conversation()
+            reset_wifi()
     elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
         sys.exit(1)
 
